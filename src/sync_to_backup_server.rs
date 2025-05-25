92819cfdaff03fb502efd587e2a4c4197b57541b
kuche1 <znamsiparolata4esno@abv.bg>
2025-05-25 15:17:37 +0300

    update

--- src/sync_to_backup_server.rs
+++ src/sync_to_backup_server.rs
@@ -2,7 +2,13 @@ use crate::log;
 
 use std::process::Command;
 
-// TODO untested
+const SERVICE_FILES_LOCATION: &str = "/etc/systemd/system/";
+// needs to end with `/` (I think)
+// actually, this is not the only place they can be, but this is good enough
+
+const BACKUP_SERVICE_FILES_LOCATION: &str = "etc_systemd_system";
+// located in home
+
 fn server_is_dead(error_folder: &String, ip: &String) -> bool {
     // ping -c 1 IP
 
@@ -18,7 +24,7 @@ fn server_is_dead(error_folder: &String, ip: &String) -> bool {
         log::err(
             error_folder,
             &format!(
-                "could ping failure for server `{}`: {}; stderr=`{}`",
+                "ping failure for server `{}`: {}; stderr=`{}`",
                 ip,
                 cmd.status,
                 String::from_utf8_lossy(&cmd.stderr)
@@ -31,9 +37,46 @@ fn server_is_dead(error_folder: &String, ip: &String) -> bool {
 }
 
 // TODO untested
-// TODO implement
+fn copy_service_files(error_folder: &String, server_ip: &String, server_user: &String) {
+    println!("copying service files...");
+
+    let cmd = match Command::new("rsync")
+        .args([
+            "-av",
+            "--delete-after",
+            "--bwlimit=20480", // 20MiB
+            SERVICE_FILES_LOCATION,
+            &format!(
+                "{}@{}:{}",
+                server_user, server_ip, BACKUP_SERVICE_FILES_LOCATION
+            ),
+        ])
+        .output()
+    {
+        Ok(v) => v,
+        Err(err) => {
+            log::err(error_folder, &format!("could not call rsync: {}", err));
+            return;
+        }
+    };
+
+    if !cmd.status.success() {
+        log::err(
+            error_folder,
+            &format!(
+                "rsync failure for server `{}` user `{}`: {}; stderr=`{}`",
+                server_ip,
+                server_user,
+                cmd.status,
+                String::from_utf8_lossy(&cmd.stderr)
+            ),
+        );
+        return;
+    }
+}
+
+// TODO untested
 pub fn main(error_folder: &String, server_ip: &String, server_user: &String) {
-    // ping server
     if server_is_dead(error_folder, server_ip) {
         log::err(
             error_folder,
@@ -42,8 +85,7 @@ pub fn main(error_folder: &String, server_ip: &String, server_user: &String) {
         return;
     }
 
-    println!("copying service metadata...");
-    // copy services
+    copy_service_files(error_folder, server_ip, server_user);
 
-    // copy files
+    // TODO copy data
 }
