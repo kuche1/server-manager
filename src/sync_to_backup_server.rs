fb3ac85609a2acc44238183327353b2c012181c3
kuche1 <znamsiparolata4esno@abv.bg>
2025-05-25 15:06:07 +0300

    update

--- src/sync_to_backup_server.rs
+++ src/sync_to_backup_server.rs
@@ -1,9 +1,48 @@
+use std::process::Command;
+
+use crate::log;
+
 // TODO untested
+fn server_is_dead(error_folder: &String, ip: &String) -> bool {
+    // ping -c 1 IP
+
+    let cmd = match Command::new("ping").args(["-c", "1", ip]).output() {
+        Ok(v) => v,
+        Err(err) => {
+            log::err(error_folder, &format!("could not call ping: {}", err));
+            return true;
+        }
+    };
+
+    if !cmd.status.success() {
+        log::err(
+            error_folder,
+            &format!(
+                "could ping failure for server `{}`: {}; stderr=`{}`",
+                ip,
+                cmd.status,
+                String::from_utf8_lossy(&cmd.stderr)
+            ),
+        );
+        return true;
+    }
+
+    false
+}
+
+// TODO untested
+// TODO implement
 pub fn main(error_folder: &String, server_ip: &String, server_user: &String) {
-    todo!();
-
     // ping server
+    if server_is_dead(error_folder, server_ip) {
+        log::err(
+            error_folder,
+            "could not sync to backup server, as it is dead",
+        );
+        return;
+    }
 
+    println!("copying service metadata...");
     // copy services
 
     // copy files
