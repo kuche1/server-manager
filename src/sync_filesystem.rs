21810caf4a1c9099a0a3d267d96b82936e3c7705
kuche1 <znamsiparolata4esno@abv.bg>
2025-05-26 13:38:26 +0300

    update

--- src/sync_filesystem.rs
+++ src/sync_filesystem.rs
@@ -1,23 +1,8 @@
-// use crate::log;
 use crate::term;
-
-// use std::process::Command;
 
 pub fn main(error_folder: &String) {
     println!("sync filesystem: working...");
 
-    //     let cmd = match Command::new("sync").output() {
-    //         Ok(v) => v,
-    //         Err(err) => {
-    //             log::err(error_folder, &format!("could call sync: {}", err));
-    //             return;
-    //         }
-    //     };
-    //
-    //     if !cmd.status.success() {
-    //         log::err(error_folder, "sync failure");
-    //         return;
-    //     }
     term::exec(error_folder, "sync", vec![], "sync filesystem");
 
     println!("sync filesystem: done!");
