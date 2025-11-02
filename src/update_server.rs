7d7cb5385b00814518af0fd1728273cc900c299c
kuche1 <znamsiparolata4esno@abv.bg>
2025-11-02 17:45:55 +0200

    update

--- src/update_server.rs
+++ src/update_server.rs
@@ -7,29 +7,35 @@ pub fn main(error_folder: &String, update_server_debian: bool) {
 
     println!("update_server: debian: fetch updates: working...");
 
-    let res = term::exec(
+    let output = term::exec(
         error_folder,
         "apt-get",
         vec!["update"],
         "debian: fetch updates",
     );
-    if res.is_none() {
-        return;
-    }
+    let output = match output {
+        Some(v) => v,
+        None => return,
+    };
+    let output = String::from_utf8_lossy(&output.stdout); // TODO: do not use the lossy version
+    println!("{}", output);
 
     println!("update_server: debian: fetch updates: done!");
 
     println!("update_server: debian: apply updates: working...");
 
-    let res = term::exec(
+    let output = term::exec(
         error_folder,
         "apt-get",
         vec!["upgrade", "--yes"],
         "debian: apply updates",
     );
-    if res.is_none() {
-        return;
-    }
+    let output = match output {
+        Some(v) => v,
+        None => return,
+    };
+    let output = String::from_utf8_lossy(&output.stdout); // TODO: do not use the lossy version
+    println!("{}", output);
 
     println!("update_server: debian: apply updates: done!");
 }
